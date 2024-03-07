4785e938e3b8f07eb7380cd17814f61835dd2c31
Fahri Wal Fahruddin <89997450+Fahriiiyyy@users.noreply.github.com>
2024-03-07 11:58:22 +0000

    Project chatbot

--- worker.py
+++ worker.py
@@ -22,7 +22,7 @@ llm_embeddings = None
 def init_llm():
     global llm, llm_embeddings
     # Initialize the language model with the OpenAI API key
-    api_key="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"
+    api_key="YOUR API KEYS"
     # ---> TODO: write your code here <----
     llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=api_key)
     # Initialize the embeddings for the language model
